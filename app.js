8621bbbaaef695f4f42c806c7917c07928216c31
dogg11111 <60827617+dogg11111@users.noreply.github.com>
2021-06-24 15:44:00 +0300

    первая версия сайта

---
+++ app.js
@@ -0,0 +1,54 @@
+// 1 Переменные
+// camelCase
+// const firstName = 'Vladilen'
+
+// const age = 26 //number
+// const isProgrammer = true // boolean
+
+// const _private = 'private'
+// const $ = 'some value'
+
+//const if = 'mkef' // err
+// const withNun5 = '5'
+// const 5withNim = '5' // err
+
+// 2 Мутирование
+// console.log('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+// alert('Имя человека: ' + firstName + ', а возраст человека: ' + age)
+ // const lastName = prompt('Введите фамилию')
+ // alert('Привет: ' + firstName + ' ' + lastName)
+ 
+// Операторы
+// let currenYear = 2021
+// const birthYear = 1991
+
+//const age = currenYear + birthYear
+
+// const a = 10
+// const b = 5
+
+// let c = 32
+// c = c+a
+// c += a // c+a
+
+// console.log(a + b)
+// console.log(a - b)
+// console.log(a * b)
+// console.log(a / b)
+// console.log(++currenYear)
+// console.log(--currenYear)
+// console.log(c)
+
+//Типы данных
+
+const isProgrammer = true
+const name = 'Vladilen'
+const age = 26
+let x
+null
+
+console.log(typeof isProgrammer)
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof x)
+console.log(null)
